3351cf3baf34233e611ce3ac4ba412e2f492140d
davidzhuprime <davidzhuprime@yahoo.com>
2023-04-09 00:36:11 +0800

    a

--- functions/redisHandler.js
+++ functions/redisHandler.js
@@ -1,14 +1,14 @@
-const redis = require('redis');
-const client = redis.createClient(); // create a Redis client
+const client = redis.createClient();
 
-// handle the click event on your web page
-document.addEventListener('click', () => {
-    // increment the click count in Redis
-    client.incr('clicks', (err, reply) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(`Number of clicks: ${reply}`);
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', () => {
+        client.incr('clicks', (err, reply) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(`Number of clicks: ${reply}`);
+            }
+        });
     });
 });
