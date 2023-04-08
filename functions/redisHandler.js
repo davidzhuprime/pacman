950dbd86e45cab7f9850f30d5af50b9f30128d59
davidzhuprime <davidzhuprime@yahoo.com>
2023-04-09 00:32:24 +0800

    a

--- functions/redisHandler.js
+++ functions/redisHandler.js
@@ -1,33 +1,14 @@
-const Redis = require('ioredis');
+const redis = require('redis');
+const client = redis.createClient(); // create a Redis client
 
-exports.handler = async (event, context) => {
-    const redis = new Redis({
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-        password: process.env.REDIS_PASSWORD,
+// handle the click event on your web page
+document.addEventListener('click', () => {
+    // increment the click count in Redis
+    client.incr('clicks', (err, reply) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`Number of clicks: ${reply}`);
+        }
     });
-
-    try {
-        // Perform your desired Redis operations here, e.g., setting and getting a key-value pair
-        await redis.set('myKey', 'Hello, Redis!');
-        const value = await redis.get('myKey');
-
-        // Close the connection
-        redis.disconnect();
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: `Redis value: ${value}` }),
-        };
-    } catch (error) {
-        console.error(error);
-
-        // Close the connection
-        redis.disconnect();
-
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Error connecting to Redis' }),
-        };
-    }
-};
+});
